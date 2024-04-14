b281db08902b6668444e6d0f68d3474581e20f9d
Jofel Salado <saladojofel@gmail.com>
2024-04-14 21:55:35 +0800

    fix: changed file path of the video

--- app/_components/background-video.tsx
+++ app/_components/background-video.tsx
@@ -19,10 +19,20 @@ export function BackgroundVideo() {
         className=" absolute top-0 z-[-1] w-full h-full object-cover visible dark:invisible"
       >
         <source
-          src="/day.mp4"
+          src="/assets/videos/night.mp4"
           type="video/mp4"
         />
-        Your browser does not support the video tag.
+      </video>
+      <video
+        autoPlay
+        loop
+        muted
+        className=" absolute top-0 z-[-1] w-full h-full object-cover invisible dark:visible"
+      >
+        <source
+          src="/assets/videos/day.mp4"
+          type="video/mp4"
+        />
       </video>
       {/* <video
         autoPlay
