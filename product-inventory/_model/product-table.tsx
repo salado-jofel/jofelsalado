fc51212bf4848d2f77c28b3306d67a4518c99229
Jofel Salado <saladojofel@gmail.com>
2024-04-14 20:30:40 +0800

    fix: removed product inventory

--- product-inventory/_model/product-table.tsx
+++
@@ -1,9 +0,0 @@
-export interface ProductTable {
-  key: string;
-  productName: string;
-  material: string;
-  category: string;
-  price: string;
-  inventory: string;
-  dateAdded: string;
-}
--- product-inventory/_model/sortby.tsx
+++
@@ -1,4 +0,0 @@
-export interface SortBy {
-  id: number;
-  name: string;
-}
