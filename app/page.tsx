e0be4c089c7f3f317d9cdcc48ef5ae379008b469
Jofel Salado <saladojofel@gmail.com>
2024-04-14 21:22:01 +0800

    fix: renamed Home to Page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "Jofel Salado",
   description: "My Portfolio",
 };
-export default function Home() {
+export default function Page() {
   return (
     <main className="h-screen w-screen relative bg-black/50">
       <div className=" w-full h-screen overflow-auto no-sc ">
