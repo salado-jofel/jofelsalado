9c5a10a36c849b51448ce36104b13fc576523e06
salado-jofel <saladojofel@gmail.com>
2024-04-20 22:09:32 +0800

    fix: added prop to header logo url

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -33,11 +33,15 @@ export default function Navbar() {
         <div className="h-full dark:text-white  text-[#8FD400] flex items-start justify-start w-full duration-300">
           {darkMode == false ? (
             <Image
-              unoptimized
+              blurDataURL=""
+              placeholder="blur"
+              priority={true}
+              quality={100}
+              unoptimized={true}
               width={2000}
               height={346}
               src={"/assets/images/logodark.png"}
-              alt={""}
+              alt={"logo"}
               className="object-contain w-[130px]  md:w-[170px] h-full duration-300"
             />
           ) : (
