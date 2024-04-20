8417dfc4cb8ce610340a24b47201ccec3f0ff173
Jofel Salado <saladojofel@gmail.com>
2024-04-21 01:55:50 +0800

    fix: added pdf file and download button

--- app/resume/_sections/resume-viewer.tsx
+++ app/resume/_sections/resume-viewer.tsx
@@ -3,5 +3,18 @@ import resume from "../../../public/assets/images/jofelsalado.png";
 import ResumeFrame from "../_components/resume-frame";
 
 export default function ResumeViewer() {
-  return <ResumeFrame resumePath={resume} />;
+  return (
+    <div className="w-full flex flex-col items-center justify-center gap-[50px]">
+      <ResumeFrame resumePath={resume} />
+      <a
+        className="duration-300 scale-100 hover:scale-125"
+        href="/assets/jofelsalado.pdf"
+        download
+      >
+        <div className="py-[12px] px-[16px] bg-[#8FD400] shadow-lg whitespace-nowrap font-roboto text-[14px] font-[800] rounded-md text-white dark:bg-white dark:text-black">
+          Download PDF
+        </div>
+      </a>
+    </div>
+  );
 }
